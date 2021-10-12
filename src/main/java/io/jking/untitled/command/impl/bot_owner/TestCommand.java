ed41bccbdcbd4fc384cb1070d411c855cbd0d3b3
VCSJacob <jkingster@protonmail.com>
2021-10-12 01:42:43 -0500

    Changed to ephemeral just to test it.

--- src/main/java/io/jking/untitled/command/impl/bot_owner/TestCommand.java
+++ src/main/java/io/jking/untitled/command/impl/bot_owner/TestCommand.java
@@ -3,21 +3,17 @@ package io.jking.untitled.command.impl.bot_owner;
 import io.jking.untitled.command.Category;
 import io.jking.untitled.command.Command;
 import io.jking.untitled.command.CommandContext;
-import net.dv8tion.jda.api.interactions.InteractionHook;
 
-import java.util.concurrent.TimeUnit;
 
 public class TestCommand extends Command {
     public TestCommand() {
-        super("test", "A testing command for the bot owners.", Category.BOT_OWNER);
+        super("test", "A testing command for the bot owners. Maybe...?", Category.BOT_OWNER);
     }
 
     @Override
     public void onCommand(CommandContext ctx) {
-        ctx.reply("I am working... hopefully?")
+        ctx.reply("I am working.. I think?")
                 .setEphemeral(true)
-                .delay(10, TimeUnit.SECONDS)
-                .flatMap(InteractionHook::deleteOriginal)
                 .queue();
     }
 }
