9a3263d4ced255c3df0734d9d64bdab3c3cd9c46
VCSJacob <jkingster@protonmail.com>
2021-10-15 20:13:56 -0500

    Info class

---
+++ src/main/java/io/jking/untitled/core/UntitledInfo.java
@@ -0,0 +1,11 @@
+package io.jking.untitled.core;
+
+import java.time.OffsetDateTime;
+
+public final class UntitledInfo {
+    public static final OffsetDateTime START_TIMESTAMP = OffsetDateTime.now();
+    public static final String VERSION = "0.0.1";
+    public static final String GITHUB_REPO = "https://github.com/JacobDevelopment/SchoolBot";
+    private UntitledInfo() {
+    }
+}
