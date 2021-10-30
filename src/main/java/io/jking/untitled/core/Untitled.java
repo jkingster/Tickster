bb7d55b8181d6cc96e1e50a9dea1771f66f8f217
VCSJacob <jkingster@protonmail.com>
2021-10-29 21:06:54 -0500

    Added dev mode versus production mode, for testing commands specifically rather than updating globally.

--- src/main/java/io/jking/untitled/core/Untitled.java
+++ src/main/java/io/jking/untitled/core/Untitled.java
@@ -1,8 +1,10 @@
 package io.jking.untitled.core;
 
 
+import io.jking.untitled.cache.Cache;
 import io.jking.untitled.command.CommandRegistry;
 import io.jking.untitled.event.*;
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
@@ -14,6 +16,14 @@ public class Untitled {
 
     private final Config config;
 
+    private boolean devMode;
+
+    private Untitled(Config config, boolean devMode) throws LoginException, InterruptedException {
+        this.config = config;
+        this.devMode = devMode;
+        startInstance();
+    }
+
     private Untitled(Config config) throws LoginException, InterruptedException {
         this.config = config;
         startInstance();
@@ -23,12 +33,17 @@ public class Untitled {
         new Untitled(config);
     }
 
+    public static void build(Config config, boolean devMode) throws LoginException, InterruptedException {
+        new Untitled(config, devMode);
+    }
+
     private void startInstance() throws LoginException, InterruptedException {
         Checks.notNull(config, "Config");
 
         final String token = config.getObject("bot").getString("token");
         Checks.notEmpty(token, "Token");
 
+        final Cache cache = new Cache();
         final CommandRegistry commandRegistry = new CommandRegistry();
 
         MessageEvent messageEvent = new MessageEvent(config);
@@ -38,12 +53,21 @@ public class Untitled {
                 .disableCache(CacheFlag.VOICE_STATE, CacheFlag.EMOTE)
                 .addEventListeners(
                         new SlashEvent(commandRegistry, config, messageEvent),
-                        new StartEvent(commandRegistry),
+                        new StartEvent(commandRegistry, cache, this),
                         new InteractionEvent(commandRegistry),
                         new InviteEvent(config),
                         messageEvent
                 )
                 .build()
                 .awaitReady();
+
+    }
+
+    public boolean isDevMode() {
+        return devMode;
+    }
+
+    public Config getConfig() {
+        return config;
     }
 }
