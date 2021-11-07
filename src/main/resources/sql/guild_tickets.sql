25b2736b747ea0dda817a7890efdee2cba27d4b9
VCSJacob <jkingster@protonmail.com>
2021-11-07 15:48:07 -0600

    Rebranding completely. This branch is a fresh-start. All other branches are deprecated/unused and will be deleted after the main push.

--- src/main/resources/sql/guild_tickets.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS guild_tickets
-(
-    id            INT PRIMARY KEY NOT NULL,
-    guild_id      BIGINT          NOT NULL,
-    creator_id    BIGINT          NOT NULL,
-    ticket_text   TEXT            NOT NULL,
-    timestamp     TEXT            NOT NULL,
-    ticket_number INT             NOT NULL
-);
