3e64b4ae079075cd3f545aa109f4b98dc4bd07a0
VCSJacob <jkingster@protonmail.com>
2021-11-10 15:09:58 -0600

    New/adjusted tables.

--- src/main/resources/sql/guild_tickets.sql
+++ src/main/resources/sql/guild_tickets.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS guild_tickets
 (
-    id          SERIAL PRIMARY KEY NOT NULL,
-    guild_id    BIGINT             NOT NULL,
-    channel_id  BIGINT             NOT NULL,
-    category_id BIGINT             NOT NULL,
-    creator_id  BIGINT             NOT NULL,
-    transcript  TEXT default 'Transcript not set.'
+    id               SERIAL PRIMARY KEY NOT NULL,
+    guild_id         BIGINT             NOT NULL,
+    channel_id       BIGINT             NOT NULL,
+    category_id      BIGINT             NOT NULL,
+    creator_id       BIGINT             NOT NULL,
+    ticket_timestamp TEXT               NOT NULL,
+    transcript       json
 );
